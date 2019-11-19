d25cd2c6a5b47493e5ee84e5732609900e5b3c95
Jean Saunie <jean.saunie@devinci.fr>
2019-11-20 00:56:52 +0100

    Correction

--- struc-conditionnelle/app.js
+++ struc-conditionnelle/app.js
@@ -2,15 +2,11 @@
  * Exercice 1 :
  * Écrire un code qui affiche en fonction de l’âge, majeur ou mineur.
  */
-    let age=prompt("quel est votre age?")
-    if(age >=18){
+    let age = +prompt("quel est votre age?") // Converti la donnée qui est une string en une valeur de type number
+    if(age >= 18){
         alert("majeur");
-    }
-    else if(age <= 18){
-        alert("mineur");
-    }
-    else{
-        alert("Heu");
+    } else  {
+        alert('mineur');
     }
 
 /**
@@ -19,16 +15,16 @@
  * Bonsoir (entre 17 et 22) et Bonne nuit (entre 22 et 8) et par défaut il affichera  Heuu.
  */
     let heure = prompt("quel heure il est ?");
-    if(heure >=0 && heure<=8){
+    if(heure >= 0 && heure <= 8){
         alert("bon réveil");
     }
-    else if(heure>=8 && heure<=17){
+    else if(heure > 8 && heure <= 17){
         alert("Bonjour");
     }
-    else if(heure>=17 && heure<=22){
+    else if(heure > 17 && heure <= 22){ // Attention : ce n'est pas >= à 17 puisqu'on test déjà l'égalité à 17 dans la condition juste au dessus
         alert("Bonsoir");
     }
-    else if(heure>=22 && heure<=8){
+    else if(heure > 22 && heure <= 8){ // Pareil ici, c'est strictement suppérieur à 22,
         alert("Bonne nuit");
     }
     else{
@@ -38,4 +34,4 @@
  * Exercice 3 :
  * Même que exercice 1, mais utilisé les conditions ternaires
  */
-    let age2=prompt("quel est votre age?")>=18 ? alert("majeur") : alert("mineur");
+    let age2= +prompt("quel est votre age?") >= 18 ? alert("majeur") : alert("mineur"); // Pareil convertit en number
