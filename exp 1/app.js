fabd1775c68afc230ae6ec93cb7de042ab33c54e
Jean Saunie <jean.saunie@devinci.fr>
2019-11-20 01:39:44 +0100

    Correction

--- exp 1/app.js
+++ exp 1/app.js
@@ -29,3 +29,46 @@ document.addEventListener('DOMContentLoaded',function () {
     });
 
 });
+
+// Correction
+// Même si la boucle for te permets de garder les 3 premier éléments, il ne sont pas trier par pertinence
+// A part ça c'est bon pour le reste
+
+// L'url de l'api a utiliser
+const url = 'https://api-adresse.data.gouv.fr/search/?q=';
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Sélectionnez le boutton n'oubliez pas de lui rajouter l'id "btn", pour pouvoir lui ajouter un event listener au click
+    document.getElementById("btn").addEventListener('click', function (e) {
+        e.preventDefault(); // On annule le comportement par default des boutons, par default un bouton recharge la page
+
+        // On récupère la valeur et on la nettoie
+        let value = document.getElementById('add').value.trim();
+
+        // N'oubliez pas de créer dans le fichier html une div ou un ul si vous créez un élément li, et de lui donner l'id list
+        const list = document.getElementById('list');
+
+        if (!!value) { // Ici on vérifie que value est différent de "", null et undefined
+            fetch(url + value) // On fait notre requête
+                .then(function (res) {
+                    return res.json();
+                })
+                .then(function (data) {
+                    list.innerHTML = ''; // On vide la list à chaque recherche
+                    
+                    const sort = data.features
+                        .sort(function (add1, add2) { // On les trie par pertinance (la propriété est "importance")
+                            return add1.properties.importance - add2.properties.importance;
+                        })
+                        .splice(0, 3); // On prend que les 3 plus pertinantes
+
+                    sort.forEach(function (add) {
+                        let item = document.createElement("P");
+                        let text = document.createTextNode(add.properties.label);
+                        item.appendChild(text);
+                        list.appendChild(item);
+                    });
+                })
+        }
+    });
+});
